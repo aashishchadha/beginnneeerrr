d86b1fd813a50b193043d4fddeccd2bf5d57276f
aashishchadha <aashish.chadha77@gmail.com>
2017-07-09 15:18:41 +0530

    Add files via upload

---
+++ sum of n natural numbers.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter value");
+		int sum;
+		int n = s.nextInt();
+		sum = (n*(n+1))/2;
+		System.out.println("the sum is="+sum);
+	}
